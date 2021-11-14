c14f1ec0018aa3414c64b2beef7d492d7fd74dc2
LGsus22 <L18TE0452@teziutlan.tecnm.mx>
2021-11-13 22:18:28 -0600

    Ingreso de nuevo nombre de main a personal

--- test1.py
+++ test1.py
@@ -1,4 +1,10 @@
 """Examen de Unidad 4
 Jesus Peralta Tapia
 """
+import Personal
 print("Archivo modificado desde pycharm")
+print("Empresa 'TraditionalStart'")
+Nombre=str(input("Ingrese su nombre: "))
+print("Seleccione su puesto dependiendo el numero:")
+print(" 1.-Jefe.\n","2.-Gerente.\n","3.-Empleado.")
+Puesto=int(input("Numero: "))
