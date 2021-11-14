35f88c2302943e96f9f22580cf7b5f33b5d04707
LGsus22 <L18TE0452@teziutlan.tecnm.mx>
2021-11-13 23:42:02 -0600

    Ultima prueba... Al fin

--- Personal.py
+++ Personal.py
@@ -8,7 +8,10 @@ class Gerente:
     def status(self):
         print("El gerente de la empresa es un empleado que mide ", self.__Altura, "mts")
         print("Se llama ", self.__Nombre, "y tiene", self.__Vestimenta)
-
+    def getInfo(self):
+        print("Nombre: ", self.__Nombre)
+        print("Altura: ", self.__Altura)
+        print("Vestimenta: ", self.__Vestimenta)
     def Entrevistando(self):
         return ("En este momento el Gerente esta entrevistando a una persona para su contratacion")
     def Supervisando(self):
@@ -18,8 +21,12 @@ class Gerente:
 class Empleado(Gerente):
     def __init__(self, Nombre, Altura, Vestimenta):
         super().__init__(Nombre, Altura, Vestimenta)
+        self.__Vestimenta = Vestimenta
+        self.__Altura = Altura
+        self.__Nombre = Nombre
     def status(self):
-        super().status()
+        print("El empleado de la empresa es un empleado que mide ", self.__Altura, "mts")
+        print("Se llama ", self.__Nombre, "y tiene", self.__Vestimenta)
     def Trabajanto(self):
         print("En este momento el empleado esta trabajando.")
     def Descansando(self):
@@ -29,8 +36,14 @@ class Empleado(Gerente):
 class Persona(Gerente):
     def __init__(self, Nombre, Altura, Vestimenta):
         super().__init__(Nombre, Altura, Vestimenta)
+        self.__Vestimenta = Vestimenta
+        self.__Altura = Altura
+        self.__Nombre = Nombre
+    def getInfo(self):
+        super().getInfo()
     def status(self):
-        super().status()
+        print("La persona va a solicitar empleo a la empresa, es una persona que mide ", self.__Altura, "mts")
+        print("Se llama ", self.__Nombre, "y tiene", self.__Vestimenta)
     def Solicitando(self):
         print("En este momento la persona esta solicitando empleo")
     def Buscando(self):
