989f8ae580e9eac2c65bb451237bd42301f9371c
LGsus22 <L18TE0452@teziutlan.tecnm.mx>
2021-11-13 23:27:07 -0600

    Tercera prueba

--- Personal.py
+++ Personal.py
@@ -1,41 +1,39 @@
 """Archivo para las clases"""
 print("Archivo modificado en pycharm")
 class Gerente:
-    def __init__(self, Nombre, Altura, Edad, Vestimenta):
+    def __init__(self, Nombre, Altura, Vestimenta):
         self.__Vestimenta=Vestimenta
         self.__Altura=Altura
-        self.__Edad=Edad
         self.__Nombre=Nombre
-    def getInfo(self):
-        print("El nombre es: ", self.__Nombre)
-        print("La edad es: ", self.__Edad)
-        print("La altura es:", self.__Altura)
-        print("La vestimenta es: ", self.__Vestimenta)
+    def status(self):
+        print("El gerente de la empresa es un empleado que mide ", self.__Altura, "mts")
+        print("Se llama ", self.__Nombre, "y tiene", self.__Vestimenta)
+
     def Entrevistando(self):
-        return ("El Gerente esta entrevistando a una persona")
+        return ("En este momento el Gerente esta entrevistando a una persona para su contratacion")
     def Supervisando(self):
-        return ("El Gerente esta supervisando las áreas de trabajo")
+        return ("En este momento el Gerente esta supervisando las áreas de trabajo")
     def Noasiste(self):
         return ("El gerente no asistio")
 class Empleado(Gerente):
-    def __init__(self, Nombre, Altura, Edad, Vestimenta):
-        super().__init__(Nombre, Altura, Edad, Vestimenta)
-    def getInfo(self):
-        super().getInfo()
+    def __init__(self, Nombre, Altura, Vestimenta):
+        super().__init__(Nombre, Altura, Vestimenta)
+    def status(self):
+        super().status()
     def Trabajanto(self):
-        print("El empleado esta trabajando.")
+        print("En este momento el empleado esta trabajando.")
     def Descansando(self):
-        print("El empleado esta descansando.")
+        print("En este momento el empleado esta descansando.")
     def Cobrar(self):
-        print("El trabajador esta cobrando.")
+        print("En este momento el trabajador esta cobrando.")
 class Persona(Gerente):
-    def __init__(self, Nombre, Altura, Edad, Vestimenta):
-        super().__init__(Nombre, Altura, Edad, Vestimenta)
-    def getInfo(self):
-        super().getInfo()
+    def __init__(self, Nombre, Altura, Vestimenta):
+        super().__init__(Nombre, Altura, Vestimenta)
+    def status(self):
+        super().status()
     def Solicitando(self):
-        print("La persona esta solicitando empleo")
+        print("En este momento la persona esta solicitando empleo")
     def Buscando(self):
-        print("La persona esta buscando empleo")
+        print("En este momento la persona esta buscando empleo")
     def Aceptado(self):
         print("La persona ha sido aceptada")
